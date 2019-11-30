ab387c3064949b7ee3532cecbd0c10b48fd02427
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-30 16:54:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,9 @@ def apply_coupons(cart, coupons)
         cart_item_name[:count] += coupons[counter][:num]
         cart_item[:count] -= coupons[counter][:num]
       else 
-        
+        cart_item_name = {
+          
+        }
       end 
     end 
     counter += 1 
