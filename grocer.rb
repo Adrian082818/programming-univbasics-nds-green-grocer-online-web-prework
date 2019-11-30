d623c33cd6d1f74491d3e7c4e56f086aa73b1de7
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-30 17:41:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -77,5 +77,6 @@ end
 def checkout(cart, coupons)
  consolidate_cart = consolidate_cart(cart)
  couponed_cart = apply_coupons(consolidate_cart)
- clearanced_cart = apply_coupons(couponed_cart)
+ clearanced_cart = apply_clearance(couponed_cart)
+ 
 end
