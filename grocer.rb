dc88f94d6964930678977c2cb889ad2ba367096d
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-30 17:02:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def apply_coupons(cart, coupons)
     if cart_item && cart_item[:count] >= coupons[counter][:num]
       if cart_item_name
         cart_item_name[:count] += coupons[counter][:num]
-        # cart_item[:count] -= coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
       else 
         cart_item_name = {
           item: coupon_item_name,
